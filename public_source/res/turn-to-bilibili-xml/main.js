bf9435fab32c694c4aeb486645d857c07683f82c
Lineng Cao <lineng.ca@gmail.com>
2023-04-05 06:37:00 +0000

    chore: Cleanup

--- public_source/res/turn-to-bilibili-xml/main.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	var patt = new RegExp('cid=(.+?)&a');	
-	var cid = patt.exec(document.documentElement.innerHTML);
-	if (cid != null){
-		location = "http://comment.bilibili.com/" + cid[1] + ".xml";
-	}else{
-		alert("好像这个页面VINCE还没能找到方法")
-	}	
-})();
